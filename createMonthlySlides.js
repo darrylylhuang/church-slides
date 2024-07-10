05567cd6f5ecf2d8b7d1120e3d63bb2038c4073b
darryI <darryl.thatguy.13@gmail.com>
2024-07-10 17:56:56 -0400

    Logic clean up. Preserve slide order; determine hymn key based on if the value is a float or a string.

--- createMonthlySlides.js
+++ createMonthlySlides.js
@@ -26,10 +26,8 @@ function createSlidesForOneWeek(nextSunday) {
   const gatherPresentationIds = GlobalConstants.gatherPresentationIds;
   const binderPresentationIds = GlobalConstants.binderPresentationIds;
 
-  var slidesToAdd = [];
-
   // HYMNS
-  [
+  const [
     date,
     gatheringNum,
     psalmNum,
@@ -44,56 +42,65 @@ function createSlidesForOneWeek(nextSunday) {
     amenCond,
     lambCond,
   ] = nextSunday.slice();
-  var hymnNumList = [
+
+  const hymnNumList = [
     gatheringNum,
     psalmNum,
     offertoryNum,
     communionNum,
     recessionalNum,
   ];
-  var hymnIdList = [];
-  for (let j = 0; j < hymnNumList.length; j++) {
-    hymnKey = hymnNumList[j];
-    if (Number.isNaN(hymnKey)) {
-      hymnIdList.push(binderPresentationIds[hymnKey]);
-    } else {
-      hymnIdList.push(gatherPresentationIds[hymnKey]);
-    }
-  }
+
+  const hymnIdList = hymnNumList.map((key) =>
+    Number.isNaN(key) ? binderPresentationIds[key] : gatherPresentationIds[key]
+  );
+
+  const [gatheringId, psalmId, offertoryId, communionId, recessionalId] =
+    hymnIdList;
+
+  // **************************************************** SLIDE ORDER ****************************************************
+  // Array to store the slides to add in a specific order
+  let slidesToAdd = [
+    { id: gatheringId },
+    { id: GlobalConstants.gloriaId, condition: gloriaCond },
+    { id: psalmId },
+    { id: GlobalConstants.gospelId, condition: gospelCond },
+    { id: GlobalConstants.lentenId, condition: lentenCond },
+    { id: offertoryId },
+    { id: GlobalConstants.sanctusId, condition: sanctusCond },
+    { id: GlobalConstants.memorial1Id, condition: memorialCond === 1 },
+    { id: GlobalConstants.memorial2Id, condition: memorialCond === 2 },
+    { id: GlobalConstants.memorial3Id, condition: memorialCond === 3 },
+    { id: GlobalConstants.amenId, condition: amenCond },
+    { id: GlobalConstants.lambId, condition: lambCond },
+    { id: communionId },
+    { id: recessionalId },
+  ];
+
+  // Filter the slides array to remove those that have a false condition
+  slidesToAdd = slidesToAdd.filter(
+    (slide) => slide.condition === undefined || slide.condition
+  );
+
+  // Map the array to extract the IDs
+  slidesToAdd = slidesToAdd.map((slide) => slide.id);
 
   // needs the date from nextSunday slice
   var newPresentation = createPresentationInFolder(date);
   var presentation = newPresentation;
   presentation.getSlides()[0].remove();
 
-  [gatheringId, psalmId, offertoryId, communionId, recessionalId] = hymnIdList;
-
-  // **************************************************** SLIDE ORDER ****************************************************
-  slidesToAdd.push(gatheringId);
-  if (gloriaCond) slidesToAdd.push(GlobalConstants.gloriaId);
-  slidesToAdd.push(psalmId);
-  if (gospelCond) slidesToAdd.push(GlobalConstants.gospelId);
-  if (lentenCond) slidesToAdd.push(GlobalConstants.lentenId);
-  slidesToAdd.push(offertoryId);
-  if (sanctusCond) slidesToAdd.push(GlobalConstants.sanctusId);
-  if (memorialCond === 1) slidesToAdd.push(GlobalConstants.memorial1Id);
-  if (memorialCond === 2) slidesToAdd.push(GlobalConstants.memorial2Id);
-  if (memorialCond === 3) slidesToAdd.push(GlobalConstants.memorial3Id);
-  if (amenCond) slidesToAdd.push(GlobalConstants.amenId);
-  if (lambCond) slidesToAdd.push(GlobalConstants.lambId);
-  slidesToAdd.push(communionId);
-  slidesToAdd.push(recessionalId);
-
-  var transitionId = GlobalConstants.transitionId;
   var presentationId = null;
 
+  // Transition slide
+  var transitionId = GlobalConstants.transitionId;
   var transitionPresentation = SlidesApp.openById(transitionId);
   var transitionSlide = transitionPresentation.getSlides()[0];
 
   // i will typically be 10 at the end of the iteration since we usually use 6 parts of the mass and 5 hymns
   for (let j = 0; j < slidesToAdd.length; j++) {
     // Find the presentation ID for nextSundayHymnNumber
-    var presentationId = slidesToAdd[j] ? slidesToAdd[j] : null;
+    presentationId = slidesToAdd[j] ? slidesToAdd[j] : null;
 
     if (presentationId) {
       // Open the existing presentation by ID
