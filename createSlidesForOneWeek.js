7d0a71a94a492cf3ebccbd3d6bbc9dee9bf3d61d
darryI <darryl.thatguy.13@gmail.com>
2024-07-11 19:33:11 -0400

    Log messages more descriptive.

--- createSlidesForOneWeek.js
+++ createSlidesForOneWeek.js
@@ -37,20 +37,40 @@ function createSlidesForOneWeek(nextSunday) {
   // **************************************************** SLIDE ORDER ****************************************************
   // Array to store the slides to add in a specific order
   let slidesToAdd = [
-    { id: gatheringId },
-    { id: GlobalConstants.gloriaId, condition: gloriaCond },
-    { id: psalmId },
-    { id: GlobalConstants.gospelId, condition: gospelCond },
-    { id: GlobalConstants.lentenId, condition: lentenCond },
-    { id: offertoryId },
-    { id: GlobalConstants.sanctusId, condition: sanctusCond },
-    { id: GlobalConstants.memorial1Id, condition: memorialCond === 1 },
-    { id: GlobalConstants.memorial2Id, condition: memorialCond === 2 },
-    { id: GlobalConstants.memorial3Id, condition: memorialCond === 3 },
-    { id: GlobalConstants.amenId, condition: amenCond },
-    { id: GlobalConstants.lambId, condition: lambCond },
-    { id: communionId },
-    { id: recessionalId },
+    { id: gatheringId, type: "Gathering" },
+    { id: GlobalConstants.gloriaId, condition: gloriaCond, type: "Gloria" },
+    { id: psalmId, type: "Psalm" },
+    {
+      id: GlobalConstants.gospelId,
+      condition: gospelCond,
+      type: "Gospel Acclamation",
+    },
+    {
+      id: GlobalConstants.lentenId,
+      condition: lentenCond,
+      type: "Lenten Gospel Acclamation",
+    },
+    { id: offertoryId, type: "Offertory" },
+    { id: GlobalConstants.sanctusId, condition: sanctusCond, type: "Sanctus" },
+    {
+      id: GlobalConstants.memorial1Id,
+      condition: memorialCond === 1,
+      type: "Memorial Acclamation 1",
+    },
+    {
+      id: GlobalConstants.memorial2Id,
+      condition: memorialCond === 2,
+      type: "Memorial Acclamation 2",
+    },
+    {
+      id: GlobalConstants.memorial3Id,
+      condition: memorialCond === 3,
+      type: "Memorial Acclamation 3",
+    },
+    { id: GlobalConstants.amenId, condition: amenCond, type: "Amen" },
+    { id: GlobalConstants.lambId, condition: lambCond, type: "Lamb of God" },
+    { id: communionId, type: "Communion" },
+    { id: recessionalId, type: "Recesional" },
   ];
 
   // Filter the slides array to remove those that have a false condition
@@ -59,14 +79,15 @@ function createSlidesForOneWeek(nextSunday) {
   );
 
   // Map the array to extract the IDs
-  slidesToAdd = slidesToAdd.map((slide) => slide.id);
+  slidesToAdd = slidesToAdd.map((slide) => ({
+    id: slide.id,
+    type: slide.type,
+  }));
 
   // needs the date from nextSunday slice
   var newPresentation = createPresentationInFolder(date);
   var presentation = newPresentation;
   presentation.getSlides()[0].remove();
-
-  var presentationId = null;
 
   // Transition slide
   var transitionId = GlobalConstants.transitionId;
@@ -76,21 +97,23 @@ function createSlidesForOneWeek(nextSunday) {
   // j will typically be 10 at the end of the iteration since we usually use 6 parts of the mass and 5 hymns
   for (let j = 0; j < slidesToAdd.length; j++) {
     // Find the presentation ID for nextSundayHymnNumber
-    presentationId = slidesToAdd[j] ? slidesToAdd[j] : null;
+    let presentationInfo = slidesToAdd[j];
+    let presentationType = presentationInfo.type;
+    let presentationId = presentationInfo.id ? presentationInfo.id : null;
 
     if (presentationId) {
       // Open the existing presentation by ID
-      Logger.log(`${j}: Copying exisiting slides for ${date}.`);
+      Logger.log(`${date}: Copying exisiting slides for ${presentationType}.`);
       var existingPresentation = SlidesApp.openById(presentationId);
       // Duplicate and append slides from existingPresentation to currentPresentation
       var slides = existingPresentation.getSlides();
       slides.forEach((slide) => copySlide(slide, presentation));
 
       // Add a transition slide after every slide added
-      Logger.log(`${j}: Adding a transition slide for ${date}.`);
+      Logger.log(`${date}: Adding a transition slide at position ${j}.`);
       presentation.appendSlide(transitionSlide);
     } else {
-      Logger.log(`Missing presentation: ${j}`);
+      Logger.log(`Missing presentation: ${presentationType}`);
     }
   }
 }
