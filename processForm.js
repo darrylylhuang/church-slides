84e11c818b2964337a7207793b15fdfebddab6ef
darryI <darryl.thatguy.13@gmail.com>
2024-07-22 19:54:16 -0400

    Send values to backend to verify.

--- processForm.js
+++ processForm.js
@@ -1,5 +1,22 @@
-function processForm() {
+function processForm(
+  gatherings,
+  psalms,
+  offertories,
+  communions,
+  recessionals,
+  gospelVerses,
+  storringtonParts
+) {
   // Process the form data as needed
+  console.log(
+    gatherings,
+    psalms,
+    offertories,
+    communions,
+    recessionals,
+    gospelVerses,
+    storringtonParts
+  );
   var response = `Hello World`;
   return response;
 }
