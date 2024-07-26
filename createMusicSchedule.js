0f6a68582b40ee71d9c44e0a265b819267c19aac
darryI <darryl.thatguy.13@gmail.com>
2024-07-26 12:36:13 -0400

    Removed redundancy in copying template.

--- createMusicSchedule.js
+++ createMusicSchedule.js
@@ -8,11 +8,6 @@ function createMusicSchedule(
   gospelVerses,
   storringtonParts
 ) {
-  // Open the template and get its body
-  const fileId = TEMPLATE_ID;
-  const template = DocumentApp.openById(fileId);
-  const templateBody = template.getBody();
-
   // The title of the new doc is this month of this year
   const today = new Date();
   const month = today.getMonth();
