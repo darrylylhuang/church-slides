c8d497340e15fcb1143cb3f3b3cb62d4e1986d39
darryI <darryl.thatguy.13@gmail.com>
2024-07-27 18:16:51 -0400

    Add a horizontal rule after every first week of a page.

--- createMusicSchedule.js
+++ createMusicSchedule.js
@@ -12,18 +12,24 @@ function createMusicSchedule(
   const today = new Date();
   const month = today.toLocaleString("en-CA", { month: "long" });
   const year = today.getFullYear();
-  // const doc = DocumentApp.create(`Music Schedule - ${month} ${year}`);
+  const doc = DocumentApp.create(`Music Schedule - ${month} ${year}`);
+
+  const body = doc.getBody();
 
   // For every Sunday, copy the template and replace the placeholders
   const numSundays = sundays.length;
-
   for (let i = 0; i < numSundays; i++) {
     // storringtonParts is a list of all the parts for the month; {name, value}
     // name will include a substring at the end of the form "-i"
-    let thisWeekStorrington = storringtonParts.filter((el) => el.name.endsWith(`-${i}`));
+    let thisWeekStorrington = storringtonParts.filter((el) =>
+      el.name.endsWith(`-${i}`)
+    );
     // once this week's parts have been filtered based on i value, strip this suffix for convenience
-    thisWeekStorrington = thisWeekStorrington.map((el) => ({name: el.name.substring(0, el.name.length - 2), value: el.value}))
-    
+    thisWeekStorrington = thisWeekStorrington.map((el) => ({
+      name: el.name.substring(0, el.name.length - 2),
+      value: el.value,
+    }));
+
     let week = {
       sunday: sundays[i],
       gathering: gatherings[i],
@@ -34,12 +40,18 @@ function createMusicSchedule(
       gospelVerse: gospelVerses[i],
       storrington: thisWeekStorrington,
     };
-  
+
     copyMusicScheudleTemplate(doc);
     insertWeeklySchedule(doc, week);
+    // Two weeks can fit on a page
+    if (i % 2 === 0) {
+      body.appendHorizontalRule();
+      // The horizontal rule adds a paragraph; remove it
+      let newParagraphChildIndex = body.getNumChildren() - 1;
+      body.removeChild(body.getChild(newParagraphChildIndex));
+    }
   }
 
   // Remove blank paragraph generated with new doc
-  const body = doc.getBody();
   body.removeChild(body.getChild(0));
 }
