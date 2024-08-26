d7f3245efe25749ae9e9097dbb960ee4704f0900
darryI <darryl.thatguy.13@gmail.com>
2024-08-26 12:53:30 -0400

    Correctly loops through weeks parameter.

--- pushGoogleSheets.js
+++ pushGoogleSheets.js
@@ -1,3 +1,19 @@
 function pushGoogleSheets(weeks) {
-  return;
+  const schedule = GlobalConstants.schedule;
+  weeks.forEach((week) => {
+    Logger.log(week);
+    Logger.log(week.sunday);
+  });
+
+  // let findThisSunday = schedule.createTextFinder(week.sunday);
+  // const thisSundayRange = findThisSunday.getCurrentMatch();
+  // const thisSundayRowIndex = thisSundayRange.getRowIndex();
+  // Logger.log(thisSundayRowIndex);
+  // gathering: gatherings[i],
+  // psalm: psalms[i],
+  // offertory: offertories[i],
+  // communion: communions[i],
+  // recessional: recessionals[i],
+  // gospelVerse: gospelVerses[i],
+  // storrington: thisWeekStorrington,
 }
