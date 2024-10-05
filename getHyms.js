524847b99f7e9d9f0e6d438cef1a9b21b92ca774
darryI <darryl.thatguy.13@gmail.com>
2024-10-05 11:01:23 -0400

    Started `getHymns.js` - initializes hymn titles for frontend now & backend later

---
+++ getHyms.js
@@ -0,0 +1,29 @@
+function getHymns() {
+  const SPREADSHEET = SpreadsheetApp.openById(SHEET_ID);
+  const GATHER_COMPREHENSIVE = SPREADSHEET.getSheetByName(
+    "Gather Comprehensive"
+  ); // Open specific sheet with Gather Comprehensive hymn info
+  const BINDER = SPREADSHEET.getSheetByName("Binder");
+  const gatherData = GATHER_COMPREHENSIVE.getRange("A2:D").getValues(); // Contains hymn number, name, and GoogleSlides ID
+  const binderData = BINDER.getRange("A2:B100").getValues(); // Contains hymn name, and GoogleSlides ID
+
+  const gatherComprehensiveTitles = {};
+  gatherData.forEach(function (column) {
+    gatherComprehensiveTitles[column[0]] = column[3]; // # ; name
+  });
+  GlobalConstants.gatherComprehensiveTitles = gatherComprehensiveTitles;
+
+  const binderTitles = [];
+  binderData.forEach(function (column) {
+    binderTitles.push(column[0]);
+  });
+  GlobalConstants.binderTitles = binderTitles;
+  // Code before this is necessary for setting up the global constants
+
+  const response = [];
+  // Flatten Gather Comprehensive into a list and concat binder hymns after
+  gatherComprehensiveTitles.forEach(function (key, element) {
+    response.push(`${key} - ${element}`);
+  });
+  return response.concat(binderTitles);
+}
